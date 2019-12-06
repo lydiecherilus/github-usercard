0d2f4aa6580bbc26e3940041b55429dd596d59ba
lydiecherilus <lydiecherilus@yahoo.fr>
2019-12-05 22:22:12 -0500

    All changes

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,6 +1,6 @@
 /* Step 1: using axios, send a GET request to the following URL 
            (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
+           https://api.github.com/users/lydiecherilus>
 */
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
@@ -24,7 +24,7 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+// const followersArray = [NolanPic, Matt-GitHub, JvyJay, kroaix, tetondan];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -53,3 +53,75 @@ const followersArray = [];
   luishrd
   bigknell
 */
+
+
+function createCards(object) {
+  // define new elements
+  const card = document.createElement('div');
+  const userImage = document.createElement('img');
+  const info = document.createElement('div');
+  const name = document.createElement('h3');
+  const userName = document.createElement('p');
+  const location = document.createElement('p');
+  const profile = document.createElement('p');
+  const profileLink = document.createElement('a');
+  const followers = document.createElement('p');
+  const following = document.createElement('p');
+  const bio = document.createElement('p');
+
+  // set class names
+  card.classList.add('card');
+  info.classList.add('card-info');
+  name.classList.add('name');
+  userName.classList.add('username');
+
+  // setup structure of elements
+  card.appendChild(userImage);
+  card.appendChild(info);
+  info.appendChild(name);
+  info.appendChild(userName);
+  info.appendChild(location);
+  info.appendChild(profile);
+  info.appendChild(profileLink);
+  info.appendChild(followers);
+  info.appendChild(following);
+  info.appendChild(bio);
+
+  // set text content
+  userImage.src = object.avatar_url
+  location.textContent = `Location: ${object.location}`;
+  name.textContent = object.name
+  userName.textContent = object.login
+  profile.textContent = `Profile: ${object.url}`;
+  followers.textContent = `Followers: ${object.followers}`;
+  following.textContent = `Following: ${object.following}`;
+  bio.textContent = `Bio: ${object.bio}`;
+
+  return card;
+}
+
+const cards = document.querySelector('.cards')
+axios.get('https://api.github.com/users/lydiecherilus')
+  .then(response => {
+    console.log(response.data);
+    const information = response.data;
+    const newCard = createCards(information)
+    cards.appendChild(newCard)
+  })
+
+const followersArray = [
+  'Matt-GitHub',
+  'NolanPic',
+  'JvyJay',
+  'kroaix',
+  'tetondan',
+];
+
+axios.get('https://api.github.com/users/lydiecherilus/followers')
+  .then(response => {
+    console.log(response.data);
+    response.data.forEach(data => {
+      const followerscard = createCards(data)
+      cards.appendChild(followerscard);
+    })
+  })
